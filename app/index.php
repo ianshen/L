9d199afb671271504bb8586f01a31b40aef3a35c
ianshen <ian.shen.sense@gmail.com>
2013-01-31 16:42:24 +0800

    2

--- app/index.php
+++ app/index.php
@@ -6,8 +6,8 @@ ini_set ( 'display_errors', 'on' );
 date_default_timezone_set ( 'Asia/Shanghai' );
 //定义关键目录
 define ( 'ROOT', dirname ( __DIR__ ) );
-define ( 'LIBS', ROOT . '/libs' );
+define ( 'LiteMVC', ROOT . '/LiteMVC' );
 define ( 'APP', __DIR__ );
-require LIBS . '/app.php';
+require LiteMVC . 'core/app.php';
 
 App::run ();
